49eab4b64882d63b8a40a544ab4a77317aeb60b6
Sandeep Martha <sandeepvarma508211@gmail.com>
2022-06-11 11:40:27 +0530

    Merge remote-tracking branch 'origin/main'

--- launch-instances/launch.sh
+++ launch-instances/launch.sh
@@ -13,7 +13,7 @@ LVER=1
 DNS_UPDATE(){
  PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1 )
  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
- aws route53 change-resource-record-sets --hosted-zone-id Z10328042UTHO0TDQJ86F --change-batch file:///tmp/record.json | jq
+ aws route53 change-resource-record-sets --hosted-zone-id Z1019300110H5BYRDJOPS --change-batch file:///tmp/record.json | jq
 }
 INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].state.Name | xargs -n1 )
 if [ "${INSTANCE_STATE}" = "running" ]; then
