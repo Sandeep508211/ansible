f5ac29bc27211fcb37f2c3cc97ea2396cc906610
Sandeep Martha <sandeepvarma508211@gmail.com>
2022-06-11 11:16:59 +0530

    Addedlaunchinstacecodeautomation

--- launch-instances/launch.sh
+++ launch-instances/launch.sh
@@ -7,13 +7,13 @@ if [ -z "${COMPONENT}" ]; then
   echo"Component name is needed"
 fi
 
-LID=lt-027a3c45fc8b672e2
-LVER=2
+LID=lt-020b06411441c09c1
+LVER=1
 
 DNS_UPDATE(){
  PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1 )
  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
- aws route53 change-resource-record-sets --hosted-zone-id Z09441321SXQ67F8MV9QF --change-batch file:///tmp/record.json | jq
+ aws route53 change-resource-record-sets --hosted-zone-id Z10328042UTHO0TDQJ86F --change-batch file:///tmp/record.json | jq
 }
 INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].state.Name | xargs -n1 )
 if [ "${INSTANCE_STATE}" = "running" ]; then
